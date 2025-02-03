985344bfcb758760d040bd35203adb00011d875e
Pozitivchik <maxymyurkovets@gmail.com>
2025-02-04 01:45:47 +0200

    upd

--- front/src/page/private-route.js
+++ front/src/page/private-route.js
@@ -3,7 +3,6 @@ import { Navigate } from "react-router-dom";
 import { AuthContext } from "../App";
 const PrivateRoute = ({ children }) => {
   const { currentAuth } = useContext(AuthContext);
-  console.log("=0000=", currentAuth);
   return currentAuth.token ? children : <Navigate to="/signin" />;
 };
 export default PrivateRoute;
